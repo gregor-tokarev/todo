f8926e330073b23c4c02b386693e45aeb1fa96ca
Egor <52395572+zipper123915@users.noreply.github.com>
2020-10-24 08:06:32 +0300

    Change default tasks

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ const tasks = [
         _id: '5d2ca9e2e03d40b326596aa7',
         completed: true,
         body:
-            'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-        title: 'Eu ea incididunt sunt consectetur fugiat non.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscinLorem ipsum dolor sit amet, consectetur adipiscig elit. Vestibulum malesuada mi nisi, at sagittis augue lobortis eu. Duis at lorem nibh. Aenean vel tincidunt nisl. Aenean non sollicitudin tortor. Morbi a sodales dui. Mauris urna tellus, cursus at rutrum ullamcorper, porttitor nec quam. Aenean et urna nisi. Curabitur id felis hendrerit, consequat elit euismod, faucibus odio. Suspendisse potenti. In gravida consectetur mauris ac blandit.',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscin',
     },
     {
         _id: '5d2ca9e29c8a94095c1288e0',
         completed: false,
         body:
-            'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum malesuada mi nisi, at sagittis augue lobortis eu. Duis at lorem nibh. Aenean vel tincidunt nisl. Aenean non sollicitudin tortor. Morbi a sodales dui. Mauris urna tellus, cursus at rutrum ullamcorper, porttitor nec quam. Aenean et urna nisi. Curabitur id felis hendrerit, consequat elit euismod, faucibus odio. Suspendisse potenti. In gravida consectetur mauris ac blandit.',
         title:
-            'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscin',
     },
     {
         _id: '5d2ca9e2e03d40b3232496aa7',
         completed: true,
         body:
-            'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-        title: 'Eu ea incididunt sunt consectetur fugiat non.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum malesuada mi nisi, at sagittis augue lobortis eu. Duis at lorem nibh. Aenean vel tincidunt nisl. Aenean non sollicitudin tortor. Morbi a sodales dui. Mauris urna tellus, cursus at rutrum ullamcorper, porttitor nec quam. Aenean et urna nisi. Curabitur id felis hendrerit, consequat elit euismod, faucibus odio. Suspendisse potenti. In gravida consectetur mauris ac blandit.',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscin',
     },
     {
         _id: '5d2ca9e29c8a94095564788e0',
         completed: false,
         body:
-            'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum malesuada mi nisi, at sagittis augue lobortis eu. Duis at lorem nibh. Aenean vel tincidunt nisl. Aenean non sollicitudin tortor. Morbi a sodales dui. Mauris urna tellus, cursus at rutrum ullamcorper, porttitor nec quam. Aenean et urna nisi. Curabitur id felis hendrerit, consequat elit euismod, faucibus odio. Suspendisse potenti. In gravida consectetur mauris ac blandit.',
         title:
-            'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscin',
     },
 ];
 
